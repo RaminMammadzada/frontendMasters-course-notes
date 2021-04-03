77c50e9eaac240c9f8348cc290a2b75f51f69b7e
Ramin Mammadzada <remki-design@hotmail.com>
2021-04-03 20:53:38 +0300

    Client-side GraghQL: mutation and caching.

--- client-side-graphql-in-react/client/src/pages/Pets.js
+++ client-side-graphql-in-react/client/src/pages/Pets.js
@@ -14,22 +14,57 @@ const ALL_PETS = gql`
       img
     }
   }
-`
+`;
+
+const NEW_PET = gql`
+  mutation CreateAPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
 
 export default function Pets() {
   const [modal, setModal] = useState(false)
   const { data, loading, error } = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(NEW_PET, {
+    update(cache, { data: { addPet } }) {
+      const data = cache.readQuery({
+        query: ALL_PETS
+      });
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet, ...data.pets] },
+      });
+    }
+  });
 
 
   const onSubmit = input => {
     setModal(false)
+
+    // if (newPet.loading) {
+    //   return <Loader />
+    // }
+    // if (newPet.error) {
+    //   return <p>error!!</p>
+    // }
+
+    createPet({
+      variables: {
+        newPet: input
+      }
+    });
   }
 
-  if (loading) {
+  if (loading || newPet.loading) {
     return <Loader />
   }
 
-  if (error) {
+  if (error || newPet.error) {
     return <p>error!</p>
   }
 
